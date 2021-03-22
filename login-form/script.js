64ac61aac9eff1be5d404e5f9c3458bf402a811e
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2021-03-21 22:50:08 -0300

    implement fetch for submit data

---
+++ login-form/script.js
@@ -0,0 +1,24 @@
+const init = () => {
+    const inputEmail = document.querySelector('input[type="email"]');
+    const inputPassword = document.querySelector('input[type="password"]');
+    const submitButton = document.querySelector('.login__submit'); 
+
+    if(submitButton) {
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: inputEmail.value,
+                    password: inputPassword.value
+                })
+            }).then((response) => console.log(response.json()))
+        })
+    }
+}
+
+window.onload = init;
