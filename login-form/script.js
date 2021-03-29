8d4a573a1ac7a743c3a1edd6ff1152f716fb3efc
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2021-03-29 11:52:34 -0300

    implement login validate

--- login-form/script.js
+++ login-form/script.js
@@ -1,11 +1,56 @@
 const init = () => {
+    const validateEmail = (event) => {
+        const input = event.currentTarget;
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const emailTest = regex.test(input.value);
+
+        if(!emailTest) {
+            submitButton.setAttribute("disabled", "disabled");
+            input.nextElementSibling.classList.add('error');
+        } else {
+            submitButton.removeAttribute("disabled");
+            input.nextElementSibling.classList.remove('error');
+        }
+    }
+
+    const validatePassowrd = (event) => {
+        const input = event.currentTarget;
+
+        if(input.value.length < 8) {
+            submitButton.setAttribute("disabled", "disabled");
+            input.nextElementSibling.classList.add('error');
+        } else {
+            submitButton.removeAttribute("disabled");
+            input.nextElementSibling.classList.remove('error');
+        }
+    }
+    
     const inputEmail = document.querySelector('input[type="email"]');
     const inputPassword = document.querySelector('input[type="password"]');
     const submitButton = document.querySelector('.login__submit');
 
+    inputEmail.addEventListener('input', validateEmail);
+    inputPassword.addEventListener('input', validatePassowrd);
+
+    const errorHandler = () => {
+        submitButton.classList.remove('loading');
+        submitButton.classList.remove('success');
+        submitButton.classList.add('error');
+        submitButton.textContent = "Error :(";
+    }
+
+    const successHandler = () => {
+        submitButton.classList.remove('loading');
+        submitButton.classList.remove('error');
+        submitButton.classList.add('success');
+        submitButton.textContent = "Sent! :)";
+    }
+
     if(submitButton) {
         submitButton.addEventListener('click', (event) => {
             event.preventDefault();
+
+            submitButton.textContent = "Loading...";
 
             fetch('https://reqres.in/api/login', {
                 method: 'POST',
@@ -17,9 +62,14 @@ const init = () => {
                     password: inputPassword.value,
                 })
             }).then((response) => {
-                return response.json();
-            }).then((data) => {
-                console.log(data)
+                if (response.status !== 200) {
+                    return errorHandler();
+                }
+                
+                successHandler();
+                
+            }).catch(() => {
+                errorHandler();
             })
         })
     }
