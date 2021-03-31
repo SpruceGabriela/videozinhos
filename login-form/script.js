4e8a3856a6ec22daf7fbc841aacedcba8fd245a1
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-31 13:16:57 -0300

    📦 refactor: fix misspelling function name

--- login-form/script.js
+++ login-form/script.js
@@ -1,78 +1,79 @@
 const init = () => {
-    const validateEmail = (event) => {
-        const input = event.currentTarget;
-        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        const emailTest = regex.test(input.value);
+  const validateEmail = (event) => {
+    const input = event.currentTarget;
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailTest = regex.test(input.value);
 
-        if(!emailTest) {
-            submitButton.setAttribute("disabled", "disabled");
-            input.nextElementSibling.classList.add('error');
-        } else {
-            submitButton.removeAttribute("disabled");
-            input.nextElementSibling.classList.remove('error');
-        }
+    if (!emailTest) {
+      submitButton.setAttribute("disabled", "disabled");
+      input.nextElementSibling.classList.add("error");
+    } else {
+      submitButton.removeAttribute("disabled");
+      input.nextElementSibling.classList.remove("error");
     }
+  };
 
-    const validatePassowrd = (event) => {
-        const input = event.currentTarget;
+  const validatePassword = (event) => {
+    const input = event.currentTarget;
 
-        if(input.value.length < 8) {
-            submitButton.setAttribute("disabled", "disabled");
-            input.nextElementSibling.classList.add('error');
-        } else {
-            submitButton.removeAttribute("disabled");
-            input.nextElementSibling.classList.remove('error');
-        }
+    if (input.value.length < 8) {
+      submitButton.setAttribute("disabled", "disabled");
+      input.nextElementSibling.classList.add("error");
+    } else {
+      submitButton.removeAttribute("disabled");
+      input.nextElementSibling.classList.remove("error");
     }
-    
-    const inputEmail = document.querySelector('input[type="email"]');
-    const inputPassword = document.querySelector('input[type="password"]');
-    const submitButton = document.querySelector('.login__submit');
+  };
 
-    inputEmail.addEventListener('input', validateEmail);
-    inputPassword.addEventListener('input', validatePassowrd);
+  const inputEmail = document.querySelector('input[type="email"]');
+  const inputPassword = document.querySelector('input[type="password"]');
+  const submitButton = document.querySelector(".login__submit");
 
-    const errorHandler = () => {
-        submitButton.classList.remove('loading');
-        submitButton.classList.remove('success');
-        submitButton.classList.add('error');
-        submitButton.textContent = "Error :(";
-    }
+  inputEmail.addEventListener("input", validateEmail);
+  inputPassword.addEventListener("input", validatePassword);
 
-    const successHandler = () => {
-        submitButton.classList.remove('loading');
-        submitButton.classList.remove('error');
-        submitButton.classList.add('success');
-        submitButton.textContent = "Sent! :)";
-    }
+  const errorHandler = () => {
+    submitButton.classList.remove("loading");
+    submitButton.classList.remove("success");
+    submitButton.classList.add("error");
+    submitButton.textContent = "Error :(";
+  };
 
-    if(submitButton) {
-        submitButton.addEventListener('click', (event) => {
-            event.preventDefault();
+  const successHandler = () => {
+    submitButton.classList.remove("loading");
+    submitButton.classList.remove("error");
+    submitButton.classList.add("success");
+    submitButton.textContent = "Sent! :)";
+  };
 
-            submitButton.textContent = "Loading...";
+  if (submitButton) {
+    submitButton.addEventListener("click", (event) => {
+      event.preventDefault();
 
-            fetch('https://reqres.in/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email: inputEmail.value,
-                    password: inputPassword.value,
-                })
-            }).then((response) => {
-                if (response.status !== 200) {
-                    return errorHandler();
-                }
-                
-                successHandler();
-                
-            }).catch(() => {
-                errorHandler();
-            })
+      submitButton.textContent = "Loading...";
+
+      fetch("https://reqres.in/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: inputEmail.value,
+          password: inputPassword.value,
+        }),
+      })
+        .then((response) => {
+          if (response.status !== 200) {
+            return errorHandler();
+          }
+
+          successHandler();
         })
-    }
-}
+        .catch(() => {
+          errorHandler();
+        });
+    });
+  }
+};
 
 window.onload = init;
